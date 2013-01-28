3a9784c54bebdb0dd15b8584f8770fe92cd01e3e
Илья Спесивцев <macondos@gmail.com>
2013-01-28 11:03:45 +0300

    исправлена ошибка в плейлисте VLC (#5)

--- src/app/includes/Lms/Playlist/Modules/Xspf.php
+++ src/app/includes/Lms/Playlist/Modules/Xspf.php
@@ -9,11 +9,12 @@ class Lms_Playlist_Modules_Xspf implements Lms_Playlist_Modules_Interface {
         echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<playlist version=\"1\" xmlns=\"http://xspf.org/ns/0/\" xmlns:vlc=\"http://www.videolan.org/vlc/playlist/ns/0/\">\n<trackList>";
         foreach ($files as $file) {
             $path = $file['path'];
-            if ($path[0]!='\\') {
-                $path = rawurlencode($path);
-                $path = str_replace("%2F", "/", $path);
-                $path = str_replace("%3A", ":", $path);
+            if ($path[0]=='\\') {
+                $path = "file:" . str_replace("\\", "/", $path);
             }
+            $path = rawurlencode($path);
+            $path = str_replace("%2F", "/", $path);
+            $path = str_replace("%3A", ":", $path);
             $path = htmlspecialchars(Lms_Translate::translate('CP1251', 'UTF-8', $path));
             $name = htmlspecialchars(Lms_Translate::translate('CP1251', 'UTF-8', $file['name']));
             echo "\n<track>\n    <title>$name</title>\n    <location>$path</location>\n</track>\n";
