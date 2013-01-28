ec5118ed0e156ce63c3f43867db029cdb4f5a1bf
Илья Спесивцев <macondos@gmail.com>
2013-01-28 11:04:33 +0300

    исправлена ошибка в плейлисте Media Player Classic (#6)

--- src/app/includes/Lms/Playlist/Modules/Mpcpl.php
+++ src/app/includes/Lms/Playlist/Modules/Mpcpl.php
@@ -7,11 +7,13 @@ class Lms_Playlist_Modules_Mpcpl implements Lms_Playlist_Modules_Interface {
         header("Content-type: video/mpcpl"); 
         header('Content-Disposition: attachment; filename="playlist.mpcpl"'); 
         echo "MPCPLAYLIST\r\n";
+        $n = 1;
         foreach ($files as $i => $file) {
             if (Lms_Application::getConfig('download', 'mpcpl_convert_name_to_utf8')) {
                 $file['path'] = Lms_Translate::translate('CP1251', 'UTF-8', $file['path']);
             }
-            echo "$i,type,0\r\n$i,filename,{$file['path']}\r\n";
+            echo "$n,type,0\r\n$n,filename,{$file['path']}\r\n";
+            $n++;
         }
     }
 }
